dd7e879241ff43fd871a90cc2188289d08d5a45b
Owner <owner@Users-MacBook-Pro.local>
2020-05-28 19:32:38 -0400

    queries weren't solved

--- queries.sql
+++ queries.sql
@@ -1,9 +1,24 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select ProductName, CategoryName
+from Category 
+join Product 
+on Category.id = Product.CategoryId
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select id, companyName from [Order] Join [Shipper] 
+where [Order].ShipVia = Shipper.Id AND OrderDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select ProductName, id from [Product] Join [OrderDetail]
+ON [OrderDetail].ProductId = [Product].Id
+where [OrderDetail].OrderId = '10251' 
+order by [OrderDetail].id
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+Select Id as OrderId,
+CompanyName as CustomersCompany,
+LastName as EmployeeLastName 
+From [Order] Join Customer on [Order].CustomerId = [Customer].Id
+Join [Employee] on [Employee].Id = [Order].EmployeeId
